c5798d6fce32f47d537409a4e6ede2b65afecba2
Jailton Monteiro <jailtonmonteiro147@gmail.com>
2025-02-08 17:07:40 -0300

    Adicionar arquivos de projeto.

---
+++ ReservaFacil/Models/Reserva.cs
@@ -0,0 +1,10 @@
+﻿namespace ReservaFacil.Models
+{
+    public class Reserva
+    {
+        public int Id { get; set; }
+        public DateTime DataReserva { get; set; }
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; } = new Usuario();
+    }
+}
---
+++ ReservaFacil/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace ReservaFacil.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
